e0cc7f95b3543c3a08fb7c46291d99112920037b
jessica-patel <76851268+jessica-patel@users.noreply.github.com>
2023-11-01 13:58:47 -0400

    Create ar.js - removing p5 for demo

---
+++ ar.js
@@ -0,0 +1,17 @@
+// Register a custom A-Frame component to handle marker detection
+AFRAME.registerComponent('custom-marker', {
+    init: function () {
+        var el = this.el;
+        // This function will be called when the marker is found.
+        el.addEventListener('markerFound', function () {
+        console.log('Custom marker found!');
+        // Perform actions or animations here
+        });
+
+        // This function will be called when the marker is lost.
+        el.addEventListener('markerLost', function () {
+        console.log('Custom marker lost!');
+        // Handle marker lost event
+        });
+    }
+    });
