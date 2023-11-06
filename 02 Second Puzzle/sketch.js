18b78e33c7ec9d61af2f8cb9c2bc69001dcef60f
aranyakhurana <146229863+aranyakhurana@users.noreply.github.com>
2023-11-05 21:09:32 -0500

    Create sketch.js

---
+++ 02 Second Puzzle/sketch.js
@@ -0,0 +1,14 @@
+/* Creation & Computation
+Experiment 2, Multiscreen
+Group B: "Where's Tiger?" Game 2
+Group members: Aranya, Yu, Aishwarya, Jessica
+
+This code was written as part of a group experience for 12 or more mobile phones to look for a lost dog named Tiger.
+
+This is game 2 which displays a word in the center of the phone screen. Participants must keep their phones on a flat surface and rotate to reveal one of twelve different words, save it by tapping the screen and then combine it into a larger sentance in order to find the dog.
+
+This code uses the iOS sensor access code (with accompanying permissions.js file) shared by Nicholas Puckett in Creation & Computation, Digital Futures, M.Des, 2023-2025
+ 
+Help was taken from ChatGPT to debug problems.
+*/
+
